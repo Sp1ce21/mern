76b492510f4fb644cb2039cd6c5959d60596b2bc
Rostislav <spellmane21@gmail.com>
2022-06-09 13:30:33 +0200

    main / react structure

---
+++ client/src/components/form/CustomForm.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import useStyles from "./styles";
+
+const CustomForm = () => {
+  const classes = useStyles();
+  return <h1>FORM</h1>;
+};
+
+export default CustomForm;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-// import store from "./redux/store";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import reducers from "./reducers/index";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
 root.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
-    <App />
-    {/* </Provider> */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
